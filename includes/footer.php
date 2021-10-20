5bb2a1004a0a2e8aa595c8c5b18904bc0249a86a
Your Name <you@example.com>
2021-10-20 15:34:31 +0700

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+</div>
+		
+	<footer>
+		<div class="innertube">
+			<p>cre: Đạt Bùi</p>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Quản lý kho hàng</title>
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<script src="https://localhost/website/admin/ckeditor/ckeditor.js"></script>
+
+</head>
+
+<body>		
+
+	<header>
+		<div class="innertube">
+			<h1>Website quản lý kho hàng</h1>
+		</div>
+	</header>
+		
+	<div id="wrapper">
+	
+		<main>
+			<div id="content">
